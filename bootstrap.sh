fc96a098f61a6aa877289ed554d1223bfd1bd2b4
ethan-mcconkey <ethanmcconkey@protonmail.com>
2024-01-09 11:00:02 +0200

    fix: Renamed init.sh to boostrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,3 +5,6 @@ sudo apt update -y && sudo apt install nala -y
 
 echo "\n---------- SETTING UP NALA SERVERS ----------"
 echo "1 2 3" | sudo nala fetch -y
+
+echo "---------- UPGRADING SYSTEM WITH NALA ----------"
+sudo nala upgrade -y
