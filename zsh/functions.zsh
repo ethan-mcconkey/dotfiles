9a9ea091abcf6a46826846278fdbf8dcd4a7ab1d
ethan-mcconkey <ethanmcconkey@protonmail.com>
2024-01-10 17:09:33 +0200

    feat: Removed update plugins function

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -1,36 +1,16 @@
 #!/bin/zsh
 
 mkcd() {
+  local dirname=
+
 	if [ $1 ]; then
 		if [ -d $1 ]; then
 			cd $1
 		else
-			mkdir $1 && cd $1
+			mkdir -p $1 && cd $1
 		fi
 	else
 		read -p "Please enter a directory name: " dirname
 		mkcd $dirname
 	fi
-}
-
-update_zsh_plugins() {
-  eval "$(ssh-agent -s)"
-  ssh-add ~/.ssh/github
-
-  if [[ -d $HOME/.config/zsh/plugins ]]; then
-    rm -rf $HOME/.config/zsh/plugins/
-    if [[ -f $HOME/.config/zsh/plugins.zsh ]]; then
-      source $HOME/.config/zsh/plugins.zsh
-    else
-      echo "Unable to source \"plugins.zsh\""
-    fi
-  else 
-    if [[ -f $HOME/.config/zsh/plugins.zsh ]]; then
-      source $HOME/.config/zsh/plugins.zsh
-    else
-      echo "Unable to source \"plugins.zsh\""
-    fi
-  fi
-
-  exec zsh
 }
