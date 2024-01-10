47c97fa652d3e1a5c29a70bd536348ee167d40c6
ethan-mcconkey <ethanmcconkey@protonmail.com>
2024-01-10 21:22:21 +0200

    fix: removed install.zsh file from all app dirs

--- nvm/install.zsh
+++
@@ -1,9 +0,0 @@
-#!/bin/zsh
-
-# https://github.com/nvm-sh/nvm
-echo "---------- INSTALLING NVM ----------"
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | zsh
-
-echo "---------- INSTALLING NODE ----------"
-nvm install --lts
-nvm use --lts
--- rustup/install.zsh
+++
@@ -1,5 +0,0 @@
-#!/bin/zsh
-
-# https://rustup.rs/
-echo "---------- INSTALLING RUSTUP ----------"
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | zsh
