54811b347b2e7ba95a04e936a96b19559d06f154
ethan-mcconkey <ethanmcconkey@protonmail.com>
2024-01-14 11:49:06 +0200

    feat: added tmux config

--- bootstrap.sh
+++ bootstrap.sh
@@ -31,3 +31,4 @@ echo "\n---------- CREATING SYMLINKS ----------"
 create_symlink $DOTFILES_DIR/zsh/zshrc.zsh $HOME/.zshrc
 create_symlink $DOTFILES_DIR/starship/starship.toml $HOME/.config/starship/starship.toml
 create_symlink $DOTFILES_DIR/alacritty/alacritty.toml $HOME/.config/alacritty/alacritty.toml
+create_symlink $DOTFILES_DIR/tmux/tmux.conf $HOME/.config/tmux/tmux.conf
