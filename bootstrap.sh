92e13a569b1a617a8451815eac58f8e036b276b3
ethan-mcconkey <ethanmcconkey@protonmail.com>
2024-01-15 23:04:47 +0200

    feat: completed symlink creation

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,6 +2,10 @@
 
 DOTFILES_DIR=$(pwd)
 
+info () {
+  printf "\r  [ \033[00;34m..\033[0m ] $1\n"
+}
+
 success () {
   printf "\r\033[2K  [ \033[00;32mOK\033[0m ] $1\n"
 }
@@ -9,22 +13,27 @@ success () {
 create_symlink() {
   local src=$1
   local dest=$2
-  
-  if [ ! -f $src ]; then
-    echo "Cannot find \"$src\"."
-    return
+  local dest_dir=$(dirname $dest)
+
+  if [ ! -d $dest_dir ]; then
+    mkdir -p $dest_dir
+    info "Created a directory at \"$dest_dir\"."
   fi
 
-  if [ -L $dest ]; then
-    rm $dest
-    ln -s $src $dest
-  elif [ -f $dest ] ; then
-    mv $dest "$dest.backup"
-    ln -s $src $dest
+  if [ -f $dest ]; then
+    if [ -L $dest ]; then
+      rm $dest > /dev/null 2> info
+      ln -s $src $dest > /dev/null 2> info
+      success "Successfully symlinked \"$src\" to \"$dest\"."
+    else 
+      mv $dest "$dest.backup" > /dev/null 2> info
+      ln -s $src $dest > /dev/null 2> info
+      success "Successfully symlinked \"$src\" to \"$dest\"."
+    fi
   else
-    ln -s $src $dest
+    ln -s $src $dest > /dev/null 2> info
+    success "Successfully symlinked \"$src\" to \"$dest\"."
   fi
-  success "$(basename $dest): Successfully created a symlink."
 }
 
 create_symlink $DOTFILES_DIR/zsh/zshrc.zsh $HOME/.zshrc
@@ -32,3 +41,5 @@ create_symlink $DOTFILES_DIR/starship/starship.toml $HOME/.config/starship/stars
 create_symlink $DOTFILES_DIR/alacritty/alacritty.toml $HOME/.config/alacritty/alacritty.toml
 create_symlink $DOTFILES_DIR/tmux/tmux.conf $HOME/.config/tmux/tmux.conf
 create_symlink $DOTFILES_DIR/lvim/config.lua $HOME/.config/lvim/config.lua
+
+create_symlink $DOTFILES_DIR/optional/pypoetry/config.toml $HOME/.config/pypoetry/config.toml
