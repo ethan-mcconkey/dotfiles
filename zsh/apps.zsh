876d3d774c26a024864660c9febf6e3505143370
ethan-mcconkey <ethanmcconkey@protonmail.com>
2024-01-16 13:36:24 +0200

    feat: added init for zoxide and fzf

--- zsh/apps.zsh
+++ zsh/apps.zsh
@@ -6,3 +6,7 @@ eval "$(pyenv init -)"
 
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+
+eval "$(zoxide init zsh)"
