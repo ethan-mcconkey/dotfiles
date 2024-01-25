b8a5c6faacc7a19c941fd74c40da7cb4989954af
ethan-mcconkey <ethanmcconkey@protonmail.com>
2024-01-25 19:55:17 +0200

    feat: added more zsh completion settings

--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -8,11 +8,6 @@ path=($HOME/.local/bin/ $HOME/bin $PYENV_ROOT/bin $path)
 fpath=($ZSH_COMPLETIONS $fpath)
 cdpath=($HOME/Documents/code)
 
-autoload -Uz compinit
-compinit
-autoload -U bashcompinit
-bashcompinit
-
 setopt histignorealldups 
 setopt sharehistory
 setopt autocd
@@ -23,6 +18,10 @@ setopt auto_cd
 [[ -f $ZSH_PLUGINS/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh ]] && source $ZSH_PLUGINS/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 
 [[ -f $ZSH_PLUGINS/zsh-autocomplete/zsh-autocomplete.plugin.zsh ]] && source $ZSH_PLUGINS/zsh-autocomplete/zsh-autocomplete.plugin.zsh
+
+zstyle ':autocomplete:*' delay 0.25
+zstyle ':autocomplete:*' add-space executables aliases functions builtins reserved-words commands
+zstyle ':autocomplete:*' recent-dirs zoxide
 
 if [[ -f $DOTFILES_HOME/zsh/apps.zsh ]]; then
   source $DOTFILES_HOME/zsh/apps.zsh
@@ -36,6 +35,5 @@ if [[ -f $DOTFILES_HOME/zsh/aliases.zsh ]]; then
   source $DOTFILES_HOME/zsh/aliases.zsh
 fi
 
-zstyle ':autocomplete:*' delay 0.25
 
 eval "$(starship init zsh)"
