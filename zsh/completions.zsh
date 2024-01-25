f1c4504bfb528f89b0cbfd2933d7832cdaf1ee51
ethan-mcconkey <ethanmcconkey@protonmail.com>
2024-01-26 00:54:06 +0200

    feat: made completion commands more redundant
    Changed all completion creation commands to check if the command exists and the completion file does not exist
    
    Then the completion creation command is run

--- zsh/completions.zsh
+++ zsh/completions.zsh
@@ -2,14 +2,15 @@
 
 [[ ! -d $ZSH_COMPLETIONS ]] && mkdir -p $ZSH_COMPLETIONS
 
-[[ ! -f $ZSH_COMPLETIONS/_rustup ]] && rustup completions zsh > $ZSH_COMPLETIONS/_rustup
+(( $+commands[rustup] )) && [[ ! -f $ZSH_COMPLETIONS/_rustup ]] && rustup completions zsh > $ZSH_COMPLETIONS/_rustup
 
-[[ ! -f $ZSH_COMPLETIONS/_cargo ]] && rustup completions zsh cargo > $ZSH_COMPLETIONS/_cargo
+(( $+commands[cargo] )) && [[ ! -f $ZSH_COMPLETIONS/_cargo ]] && rustup completions zsh cargo > $ZSH_COMPLETIONS/_cargo
 
-[[ ! -f $ZSH_COMPLETIONS/_poetry ]] && poetry completions zsh > $ZSH_COMPLETIONS/_poetry
+(( $+commands[poetry] )) && [[ ! -f $ZSH_COMPLETIONS/_poetry ]] && poetry completions zsh > $ZSH_COMPLETIONS/_poetry
 
-[[ ! -f $ZSH_COMPLETIONS/_poe ]] && poe _zsh_completion > $ZSH_COMPLETIONS/_poe
+(( $+commands[poe] )) && [[ ! -f $ZSH_COMPLETIONS/_poe ]] && poe _zsh_completion > $ZSH_COMPLETIONS/_poe
 
 [[ -r $NVM_DIR/bash_completion ]] && \. $NVM_DIR/bash_completion
 
 [[ -f $PYENV_ROOT/completions/pyenv.zsh ]] && source $(pyenv root)/completions/pyenv.zsh
+
