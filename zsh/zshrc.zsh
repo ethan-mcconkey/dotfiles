0b5a1b411a7fee5926e95f0cd00843ad756cb612
ethan-mcconkey <ethanmcconkey@protonmail.com>
2024-01-26 00:56:13 +0200

    feat: added more config

--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -9,28 +9,45 @@ setopt sharehistory
 setopt autocd
 setopt auto_cd
 
+autoload -U colors && colors
+
 [[ -f $ZSH_PLUGINS/zsh-autosuggestions/zsh-autosuggestions.zsh ]] && source $ZSH_PLUGINS/zsh-autosuggestions/zsh-autosuggestions.zsh
 
 [[ -f $ZSH_PLUGINS/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh ]] && source $ZSH_PLUGINS/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 
 [[ -f $ZSH_PLUGINS/zsh-autocomplete/zsh-autocomplete.plugin.zsh ]] && source $ZSH_PLUGINS/zsh-autocomplete/zsh-autocomplete.plugin.zsh
 
+zstyle ':completion:*' cache-path $XDG_CACHE_HOME/zsh/zcompcache
 zstyle ':autocomplete:*' delay 0.25
 zstyle ':autocomplete:*' add-space executables aliases functions builtins reserved-words commands
 zstyle ':autocomplete:*' recent-dirs zoxide
 
-if [[ -f $DOTFILES_HOME/zsh/apps.zsh ]]; then
-  source $DOTFILES_HOME/zsh/apps.zsh
-fi
+[[ -f $DOTFILES_HOME/zsh/apps.zsh ]] && source $DOTFILES_HOME/zsh/apps.zsh
 
-if [[ -f $DOTFILES_HOME/zsh/completions.zsh ]]; then
-  source $DOTFILES_HOME/zsh/completions.zsh
-fi
+[[ -f $DOTFILES_HOME/zsh/completions.zsh ]] && source $DOTFILES_HOME/zsh/completions.zsh
 
-if [[ -f $DOTFILES_HOME/zsh/aliases.zsh ]]; then
-  source $DOTFILES_HOME/zsh/aliases.zsh
-fi
+[[ -f $DOTFILES_HOME/zsh/aliases.zsh ]] && source $DOTFILES_HOME/zsh/aliases.zsh
 
 bindkey -v
+
+function zle-keymap-select {
+  if [[ ${KEYMAP} == vicmd ]] ||
+     [[ $1 = 'block' ]]; then
+    echo -ne '\e[1 q'
+  elif [[ ${KEYMAP} == main ]] ||
+       [[ ${KEYMAP} == viins ]] ||
+       [[ ${KEYMAP} = '' ]] ||
+       [[ $1 = 'beam' ]]; then
+    echo -ne '\e[5 q'
+  fi
+}
+zle -N zle-keymap-select
+zle-line-init() {
+    zle -K viins
+    echo -ne "\e[5 q"
+}
+zle -N zle-line-init
+echo -ne '\e[5 q'
+preexec() { echo -ne '\e[5 q' ;}
 
 eval "$(starship init zsh)"
