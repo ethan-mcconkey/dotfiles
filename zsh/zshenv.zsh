a01b513df4d1032e62f0537197872172bf2e19bf
ethan-mcconkey <ethanmcconkey@protonmail.com>
2024-01-26 00:56:36 +0200

    feat: added more exports

--- zsh/zshenv.zsh
+++ zsh/zshenv.zsh
@@ -1,11 +1,16 @@
 #!/bin/zsh
 
+export PATH="$HOME/.local/bin:$HOME/bin:$PATH"
+export FPATH="$HOME/.config/zsh/completions:$FPATH"
+export CDPATH="$HOME/Documents/code:$CDPATH"
+
 export XDG_DATA_HOME="$HOME/.local/share"
 export XDG_CONFIG_HOME="$HOME/.config"
 export XDG_STATE_HOME="$HOME/.local/state"
 export XDG_CACHE_HOME="$HOME/.cache"
 export XDG_DATA_DIRS="/usr/local/share:/usr/share"
 export XDG_CONFIG_DIRS="/etc/xdg:$XDG_CONFIG_DIRS"
+
 export GPG_TTY=$(tty)
 export TERM="xterm-256color"
 export STARSHIP_CONFIG="$XDG_CONFIG_HOME/starship/starship.toml"
@@ -13,7 +18,16 @@ export DOTFILES_HOME="$HOME/.dotfiles"
 export ZSH_PLUGINS="$XDG_CONFIG_HOME/zsh/plugins"
 export ZSH_COMPLETIONS="$XDG_CONFIG_HOME/zsh/completions"
 export KEYTIMEOUT=1
+export ZSH_AUTOSUGGEST_STRATEGY=(history completion)
+export ZDOTDIR="$HOME/.config/zsh"
 
-[[ -f $ZSH_PLUGINS/zsh-autocomplete/zsh-autocomplete.plugin.zsh ]] && ZSH_AUTOSUGGEST_STRATEGY=(history completion)
+export CARGO_HOME="$XDG_DATA_HOME/cargo"
+export DOCKER_CONFIG="$XDG_CONFIG_HOME/docker"
+export MACHINE_STORAGE_PATH="$XDG_DATA_HOME/docker-machine"
+export NPM_CONFIG_USERCONFIG="$XDG_CONFIG_HOME/npm/npmrc"
+export PYENV_ROOT="$XDG_DATA_HOME/pyenv"
+export PYTHON_HISTORY="$XDG_STATE_HOME/python/history"
+export RUSTUP_HOME="$XDG_DATA_HOME/rustup"
+export XAUTHORITY="$XDG_RUNTIME_DIR/Xauthority"
 
-. "$HOME/.cargo/env"
+. "$CARGO_HOME/env"
