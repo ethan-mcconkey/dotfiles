73f14af60046475226fca72fc3ee08d099ed80f3
ethan-mcconkey <95688781+ethan-mcconkey@users.noreply.github.com>
2024-02-02 23:04:25 +0200

    chore: formatting

--- nvim/lua/ethan/plugins/nvim-treesitter.lua
+++ nvim/lua/ethan/plugins/nvim-treesitter.lua
@@ -1,20 +1,19 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  event = "VeryLazy",
-  build = ":TSUpdate",
-  config = function()
-    require("nvim-treesitter.configs").setup({
-      auto_install = true,
-      highlight = {
-        enable = true,
-      },
-      indent = {
-        enable = true,
-      },
-      ensure_installed = {
-        "lua",
-        "python",
-      },
-    })
-  end,
+	"nvim-treesitter/nvim-treesitter",
+	event = "VeryLazy",
+	build = ":TSUpdate",
+	config = function()
+		require("nvim-treesitter.configs").setup({
+			auto_install = true,
+			highlight = {
+				enable = true,
+			},
+			indent = {
+				enable = true,
+			},
+			ensure_installed = {
+				"lua",
+			},
+		})
+	end,
 }
