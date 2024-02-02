f56190b5b35681d1e2d6e8e6b6c8edaa9fd84af9
ethan-mcconkey <95688781+ethan-mcconkey@users.noreply.github.com>
2024-02-02 23:03:37 +0200

    fix: fixed init plugin spec
    fixed init.lua to only be used for single line preconfigured plugins to be loaded

--- nvim/lua/ethan/plugins/init.lua
+++ nvim/lua/ethan/plugins/init.lua
@@ -1,14 +1,7 @@
 return {
-  {
-    "navarasu/onedark.nvim",
-    lazy = false,
-    config = function()
-      local onedark = require("onedark")
-      onedark.setup({
-        style = "cool",
-        transparent = true,
-      })
-      onedark.load()
-    end,
-  },
+	{
+		"stevearc/dressing.nvim",
+		event = "VeryLazy",
+	},
+	"github/copilot.vim",
 }
