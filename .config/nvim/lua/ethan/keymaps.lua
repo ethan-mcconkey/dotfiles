d32b75dfe17b17e3f4a9f0578a52a6f7817dd2b2
ethan-mcconkey <95688781+ethan-mcconkey@users.noreply.github.com>
2024-02-15 12:15:23 +0200

    feat: removed keymaps that changed muscle memory too much

--- .config/nvim/lua/ethan/keymaps.lua
+++ .config/nvim/lua/ethan/keymaps.lua
@@ -3,7 +3,5 @@ vim.g.mapleader = " "
 local set = vim.keymap.set
 
 set("n", "<leader>w", "<cmd> w <CR>", { desc = "Write current buffer" })
-set("n", "<leader>q", "<cmd> q <CR>", { desc = "Quit current buffer" })
 set("n", "<leader>x", "<cmd> bd <CR>", { desc = "Close current buffer" })
 set("n", "<leader>hc", "<cmd> nohlsearch <CR>", { desc = "Remove search highlighting" })
-set("n", ";", ":", { desc = "Quick command mode" })
