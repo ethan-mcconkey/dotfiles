f1aaeb6b82693fc22994b0f1310478d4dc958533
ethan-mcconkey <95688781+ethan-mcconkey@users.noreply.github.com>
2024-02-23 10:39:27 +0200

    feat: added pip terminal completions

--- .config/zsh/completions.zsh
+++ .config/zsh/completions.zsh
@@ -17,3 +17,5 @@
 (( $+commands[tmuxinator] )) && [[ ! -f $ZDOTDIR/completions/_tmuxinator ]] && wget https://raw.githubusercontent.com/tmuxinator/tmuxinator/master/completion/tmuxinator.zsh -O $ZDOTDIR/completions/_tmuxinator
 
 (( $+commands[pipx] )) && eval "$(register-python-argcomplete pipx)"
+
+(( $+commands[pip] )) && eval "$(pip completion --zsh)"
