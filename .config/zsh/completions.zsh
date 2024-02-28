3153aaefce8c8f1c683578b4a63af38afd67bdca
ethan-mcconkey <95688781+ethan-mcconkey@users.noreply.github.com>
2024-02-28 18:28:52 +0200

    chore: reformat

--- .config/zsh/completions.zsh
+++ .config/zsh/completions.zsh
@@ -1,21 +1,28 @@
 #!/bin/zsh
 
-[[ ! -d $ZDOTDIR/completions ]] && mkdir -p $ZDOTDIR/completions
+[[ ! -d $XDG_DATA_HOME/zsh/completions ]] && mkdir -p $XDG_DATA_HOME/zsh/completions
 
-(( $+commands[rustup] )) && [[ ! -f $ZDOTDIR/completions/_rustup ]] && rustup completions zsh > $ZDOTDIR/completions/_rustup
+[[ ! -f $XDG_DATA_HOME/zsh/completions/_rustup ]] && \
+  rustup completions zsh > $XDG_DATA_HOME/zsh/completions/_rustup
 
-(( $+commands[cargo] )) && [[ ! -f $ZDOTDIR/completions/_cargo ]] && rustup completions zsh cargo > $ZDOTDIR/completions/_cargo
+[[ ! -f $XDG_DATA_HOME/zsh/completions/_cargo ]] && \
+  rustup completions zsh cargo > $XDG_DATA_HOME/zsh/completions/_cargo
 
-(( $+commands[poetry] )) && [[ ! -f $ZDOTDIR/completions/_poetry ]] && poetry completions zsh > $ZDOTDIR/completions/_poetry
+[[ ! -f $XDG_DATA_HOME/zsh/completions/_poetry ]] && \
+  poetry completions zsh > $XDG_DATA_HOME/zsh/completions/_poetry
 
-(( $+commands[poe] )) && [[ ! -f $ZDOTDIR/completions/_poe ]] && poe _zsh_completion > $ZDOTDIR/completions/_poe
+[[ ! -f $XDG_DATA_HOME/zsh/completions/_poe ]] && \
+  poe _zsh_completion > $XDG_DATA_HOME/zsh/completions/_poe
 
 [[ -f $PYENV_ROOT/completions/pyenv.zsh ]] && source $(pyenv root)/completions/pyenv.zsh
 
-(( $+commands[rg] )) && [[ ! -f $ZDOTDIR/completions/_rg ]] && rg --generate complete-zsh > $ZDOTDIR/completions/_rg
+[[ ! -f $XDG_DATA_HOME/zsh/completions/_rg ]] && \
+  rg --generate complete-zsh > $XDG_DATA_HOME/zsh/completions/_rg
 
-(( $+commands[tmuxinator] )) && [[ ! -f $ZDOTDIR/completions/_tmuxinator ]] && wget https://raw.githubusercontent.com/tmuxinator/tmuxinator/master/completion/tmuxinator.zsh -O $ZDOTDIR/completions/_tmuxinator
+[[ ! -f $XDG_DATA_HOME/zsh/completions/_tmuxinator ]] && \
+  wget https://raw.githubusercontent.com/tmuxinator/tmuxinator/master/completion/tmuxinator.zsh -O \
+  $XDG_DATA_HOME/zsh/completions/_tmuxinator
 
-(( $+commands[pipx] )) && eval "$(register-python-argcomplete pipx)"
+eval "$(register-python-argcomplete pipx)"
 
-(( $+commands[pip] )) && eval "$(pip completion --zsh)"
+eval "$(pip completion --zsh)"
