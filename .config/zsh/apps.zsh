6b33ed943b5a13dfc1c23be5a0c57344f1b2d894
ethan-mcconkey <95688781+ethan-mcconkey@users.noreply.github.com>
2024-02-28 18:29:20 +0200

    fix: removed pyenv channging prompt

--- .config/zsh/apps.zsh
+++ .config/zsh/apps.zsh
@@ -1,7 +1,9 @@
 #!/bin/zsh
 
+export PYENV_VIRTUALENV_DISABLE_PROMPT=0
 [[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
 eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
 
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
 
